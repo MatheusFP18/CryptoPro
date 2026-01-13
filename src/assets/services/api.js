799db7fd1b9f26213f1069a8ccf9d227e5be6503
MatheusFP18 <matheusfelippe15r@gmail.com>
2026-01-12 23:42:19 -0300

    Create service API

---
+++ src/assets/services/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const instace = axios.create({
+    baseURL: 'https://api.coingecko.com/api/v3/',
+    timeout: 10000
+})
+
+export const getCoins = async (currency = 'brl', page = 1) => {
+    const response = await instace.get('coins/markets', { 
+        params: {
+            vs_currency: currency,
+            page: page,
+            per_page: 10,
+            order: 'market_cap_desc',
+            sparkline: false
+        }
+    })
+    return response.data
+}
+
+export const getCoinById = async (id) => {
+    const response = await instace.get(`coins/${id}`)
+    return response.data
+}
