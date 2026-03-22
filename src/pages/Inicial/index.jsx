17c19c4807ea7a0beaec1bd079c94e7bfaab5f5e
MatheusFP18 <matheusfelippe15r@gmail.com>
2026-03-22 18:46:39 -0300

    feat: implement page layout

--- src/pages/Inicial/index.jsx
+++ src/pages/Inicial/index.jsx
@@ -1,17 +1,7 @@
-import { Box, Container, Divider } from "@mui/material";
-import { Header } from "../../components/Header";
 import { SectionMain } from "../../components/SectionMain";
-import { Sidebar } from "../../components/common/Sidebar";
 
 export const Inicial = () => {
   return (
-    <Container sx={{ display: "flex"}}>
-      <Sidebar />
-      <Box>
-      <Header />
-      <Divider />
-      <SectionMain />
-      </Box>
-    </Container>
+    <SectionMain />
   );
 };
